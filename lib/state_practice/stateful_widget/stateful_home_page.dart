5f21e4120ea87b95bae8db631f364ad4cebf15c1
Mimimi-d <mimimi.dev.1202@gmail.com>
2022-06-09 12:00:09 +0900

    create stateful_widget

---
+++ lib/state_practice/stateful_widget/stateful_home_page.dart
@@ -0,0 +1,56 @@
+// ① Stateful Widget
+import 'package:flutter/material.dart';
+import 'package:flutter_state_practice/state_practice/stateful_widget/loading_widget.dart';
+import 'package:flutter_state_practice/state_practice/stateful_widget/widget_a.dart';
+import 'package:flutter_state_practice/state_practice/stateful_widget/widget_b.dart';
+import 'package:flutter_state_practice/state_practice/stateful_widget/widget_c.dart';
+
+class StatefulWidgetPage extends StatefulWidget {
+  const StatefulWidgetPage({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return StatefulWidgetPageState();
+  }
+}
+
+// ② State
+class StatefulWidgetPageState extends State<StatefulWidgetPage> {
+  int _count = 0;
+  bool _isLoading = false;
+
+  void _increment() async {
+    setState(() {
+      _isLoading = true;
+    });
+    Future.delayed(const Duration(seconds: 1)).then((_) {
+      // ④ setStateメソッドにより状態の変更を通知
+      setState(() {
+        _count++;
+      });
+    }).whenComplete(() {
+      setState(() {
+        _isLoading = false;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('called HomePageState#build');
+    return Stack(
+      children: <Widget>[
+        Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          // ③ コンストラクタ経由で状態を受け渡し
+          children: <Widget>[
+            WidgetA(_count),
+            const WidgetB(),
+            WidgetC(_increment)
+          ],
+        ),
+        LoadingWidget(_isLoading)
+      ],
+    );
+  }
+}
